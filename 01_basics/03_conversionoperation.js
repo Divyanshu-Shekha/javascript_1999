4db48a61af930978258425935ed859a07572b43b
Divyanshu Shekhar <divyanshushekhar20@gmail.com>
2024-07-06 17:27:22 +0530

    CHANGE IN

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,15 @@
+// let str = "divyanshu";
+// let val = 12342;
+// let newl = Number(str);
+// let newl1 = String(val);
+// console.log(typeof(newl));
+// console.log(typeof(val));
+// console.log(typeof(newl1));
+
+
+// const negval = 3;
+// const fval = -negval;
+// console.log(fval);
+
+// console.log(+true);
+// console.log(true);
